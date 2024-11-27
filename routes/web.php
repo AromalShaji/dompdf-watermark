f40a7fef195461677617268f52f508f3df858cc5
AromalShaji <aromalpshaji10@gmail.com>
2024-11-27 11:44:46 +0530

    271120246

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\watermark;
 
-Route::get('/', function () {
-    return view('welcome');
-}); 
-Route::any('ard/', [watermark::class, 'ard'])->name('ard');
+
+Route::any('/', [watermark::class, 'ard'])->name('ard');
 
